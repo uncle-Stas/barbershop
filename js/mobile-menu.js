cb5da67eb277216a8d829becb6ec383fd55fa0f1
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-05-25 22:28:12 +0300

    header redy

---
+++ js/mobile-menu.js
@@ -0,0 +1,18 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    bodyHide: document.querySelector("[data-menu-body]"),
+    mobileMenu: document.querySelector('.mobile-menu-nav__list'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.mobileMenu.addEventListener('click', toggleModal)
+
+  function toggleModal() {
+    refs.menu.classList.toggle('is-open');
+    refs.bodyHide.classList.toggle("body-hide");
+  }
+})();
