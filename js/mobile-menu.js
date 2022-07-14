cd1dc957d2e65ce1a709e8b53ffe25aa582c649f
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-07-15 01:48:41 +0300

    added pretteirrc.json, footer, form and contacts

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,16 @@
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
-    bodyHide: document.querySelector("[data-menu-body]"),
-    mobileMenu: document.querySelector('.mobile-menu-nav__list'),
+    bodyHide: document.querySelector('[data-menu-body]'),
+    mobileMenu: document.querySelector('.mobile-menu'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
-  refs.mobileMenu.addEventListener('click', toggleModal)
+  refs.mobileMenu.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
-    refs.bodyHide.classList.toggle("body-hide");
+    refs.bodyHide.classList.toggle('body-hide');
   }
 })();
